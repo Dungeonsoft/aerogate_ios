37dec250ac6a48adb64352ee6c7f938e90a3928e
ernham <ernham1@gmail.com>
2015-03-10 18:37:32 +0900

    assets001

---
+++ Assets/FbOutScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FbOutScript : MonoBehaviour {
+
+    public void FbOut()
+    {
+        GameObject.Find("FacebookLogin").GetComponent<FaceBookUseScript>().CallFBLogout();
+        PlayerPrefs.DeleteAll();
+        Debug.Log("FbOut Started!!");
+        ValueDeliverScript.ResetValue();
+        StartCoroutine(LoadFirst());
+    }
+
+    public void LogOutForFbLogin()
+    {
+        PlayerPrefs.DeleteAll();
+        ValueDeliverScript.ResetValue(true);
+        StartCoroutine(LoadFirst());
+    }
+
+    IEnumerator LoadFirst()
+    {
+        yield return new WaitForSeconds(1f);
+        //Application.Quit();
+        Destroy(GameObject.Find("FacebookLogin"));
+        Destroy(GameObject.Find("EveryPlayTest"));
+        
+        Application.LoadLevel(0);
+
+    }
+}
---
+++ Assets/UnityAdsManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class UnityAdsManager : MonoBehaviour {
+
+    void Awake()
+    {
+        string adsID = "24534";
+        
+#if UNITY_IOS
+        adsID = "24763";
+#endif
+
+        if (Advertisement.isSupported)
+        {
+            Advertisement.allowPrecache = true;
+            Advertisement.Initialize(adsID, false);
+        }
+        else
+        {
+            Debug.Log("Platform not supported");
+        }
+    }
+
+
+    public void AbleAds(System.Action SucceessF = null , System.Action FailF =null)
+    {
+        Debug.Log("옴01");
+        if (Advertisement.isReady() == true)
+        {
+            Debug.Log("옴02");
+            FailF();
+            Debug.Log("옴03");
+
+            Advertisement.Show(null, new ShowOptions
+            {
+                pause = true,
+                resultCallback = result =>
+                {
+                    Debug.Log(result.ToString());
+                    if (result == ShowResult.Finished)
+                    {
+                        SucceessF();
+                    }
+                    else if(result == ShowResult.Failed || result == ShowResult.Skipped)
+                    {
+                        FailF();
+                    }
+                }
+            });
+        }
+    }
+}
