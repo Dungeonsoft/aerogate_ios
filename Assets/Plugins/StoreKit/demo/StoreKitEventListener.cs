1cc2dda7e8c73984ed1c9f0f20d51e2e5bb6338b
ernham <ernham1@gmail.com>
2015-03-16 18:14:07 +0900

    iOS_결제 모듈 추가
    iOS Prime31 추가 // 인앱 구매 코드작

---
+++ Assets/Plugins/StoreKit/demo/StoreKitEventListener.cs
@@ -0,0 +1,150 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Prime31;
+
+public class StoreKitEventListener : MonoBehaviour
+{
+	public GameObject Goods01;
+	public GameObject Goods02;
+	public GameObject Goods03;
+	public GameObject Goods04;
+	public GameObject Goods05;
+	public int productNumber;
+	public GameObject loadingWindow;
+
+#if UNITY_IPHONE
+	void OnEnable()
+	{
+		// Listens to all the StoreKit events. All event listeners MUST be removed before this object is disposed!
+		StoreKitManager.transactionUpdatedEvent += transactionUpdatedEvent;
+		StoreKitManager.productPurchaseAwaitingConfirmationEvent += productPurchaseAwaitingConfirmationEvent;
+		StoreKitManager.purchaseSuccessfulEvent += purchaseSuccessfulEvent;
+		StoreKitManager.purchaseCancelledEvent += purchaseCancelledEvent;
+		StoreKitManager.purchaseFailedEvent += purchaseFailedEvent;
+		StoreKitManager.productListReceivedEvent += productListReceivedEvent;
+		StoreKitManager.productListRequestFailedEvent += productListRequestFailedEvent;
+		StoreKitManager.restoreTransactionsFailedEvent += restoreTransactionsFailedEvent;
+		StoreKitManager.restoreTransactionsFinishedEvent += restoreTransactionsFinishedEvent;
+		StoreKitManager.paymentQueueUpdatedDownloadsEvent += paymentQueueUpdatedDownloadsEvent;
+	}
+	
+	
+	void OnDisable()
+	{
+		// Remove all the event handlers
+		StoreKitManager.transactionUpdatedEvent -= transactionUpdatedEvent;
+		StoreKitManager.productPurchaseAwaitingConfirmationEvent -= productPurchaseAwaitingConfirmationEvent;
+		StoreKitManager.purchaseSuccessfulEvent -= purchaseSuccessfulEvent;
+		StoreKitManager.purchaseCancelledEvent -= purchaseCancelledEvent;
+		StoreKitManager.purchaseFailedEvent -= purchaseFailedEvent;
+		StoreKitManager.productListReceivedEvent -= productListReceivedEvent;
+		StoreKitManager.productListRequestFailedEvent -= productListRequestFailedEvent;
+		StoreKitManager.restoreTransactionsFailedEvent -= restoreTransactionsFailedEvent;
+		StoreKitManager.restoreTransactionsFinishedEvent -= restoreTransactionsFinishedEvent;
+		StoreKitManager.paymentQueueUpdatedDownloadsEvent -= paymentQueueUpdatedDownloadsEvent;
+	}
+	
+	
+	
+	void transactionUpdatedEvent( StoreKitTransaction transaction )
+	{
+		Debug.Log( "transactionUpdatedEvent: " + transaction );
+	}
+
+	
+	void productListReceivedEvent( List<StoreKitProduct> productList )
+	{
+		Debug.Log( "productListReceivedEvent. total products received: " + productList.Count );
+		
+		// print the products to the console
+		foreach( StoreKitProduct product in productList )
+			Debug.Log( product.ToString() + "\n" );
+	}
+	
+	
+	void productListRequestFailedEvent( string error )
+	{
+		Debug.Log( "productListRequestFailedEvent: " + error );
+		loadingWindow.SetActive(false);
+	}
+	
+
+	void purchaseFailedEvent( string error )
+	{
+		Debug.Log( "purchaseFailedEvent: " + error );
+		loadingWindow.SetActive(false);
+	}
+	
+
+	void purchaseCancelledEvent( string error )
+	{
+		Debug.Log( "purchaseCancelledEvent: " + error );
+		loadingWindow.SetActive(false);
+	}
+	
+	
+	void productPurchaseAwaitingConfirmationEvent( StoreKitTransaction transaction )
+	{
+		Debug.Log( "productPurchaseAwaitingConfirmationEvent: " + transaction );
+	}
+	
+	
+	void purchaseSuccessfulEvent( StoreKitTransaction transaction )
+	{
+
+		switch (productNumber) 
+		{
+			case 1:
+				Debug.Log( "purchaseSuccessfulEvent: " + transaction );
+				Goods01.SendMessage("Purchase");
+				loadingWindow.SetActive(false);
+				break;
+			case 2:
+				Debug.Log( "purchaseSuccessfulEvent: " + transaction );
+				Goods01.SendMessage("Purchase");
+				loadingWindow.SetActive(false);
+				break;
+			case 3:
+				Debug.Log( "purchaseSuccessfulEvent: " + transaction );
+				Goods01.SendMessage("Purchase");
+				loadingWindow.SetActive(false);
+				break;
+			case 4:
+				Debug.Log( "purchaseSuccessfulEvent: " + transaction );
+				Goods01.SendMessage("Purchase");
+				loadingWindow.SetActive(false);
+				break;
+			case 5:
+				Debug.Log( "purchaseSuccessfulEvent: " + transaction );
+				loadingWindow.SetActive(false);
+				Goods01.SendMessage("Purchase");
+				break;
+		}
+	}
+	
+	
+	void restoreTransactionsFailedEvent( string error )
+	{
+		Debug.Log( "restoreTransactionsFailedEvent: " + error );
+		loadingWindow.SetActive(false);
+	}
+	
+	
+	void restoreTransactionsFinishedEvent()
+	{
+		Debug.Log( "restoreTransactionsFinished" );
+		loadingWindow.SetActive(false);
+	}
+	
+	
+	void paymentQueueUpdatedDownloadsEvent( List<StoreKitDownload> downloads )
+	{
+		Debug.Log( "paymentQueueUpdatedDownloadsEvent: " );
+		foreach( var dl in downloads )
+			Debug.Log( dl );
+	}
+	
+#endif
+}
+
---
+++ Assets/Scripts/_China/PurchaseManager_iOS.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+public class PurchaseManager_iOS : MonoBehaviour {
+
+
+	public GameObject loadingWindow;
+	// Use this for initialization
+	
+#if UNITY_IPHONE
+	void Start () 
+	{
+		bool canMakePayments = StoreKitBinding.canMakePayments ();
+		Debug.Log ("StoreKit canMakePayments: " + canMakePayments);
+		if (canMakePayments) 
+		{
+				StartCoroutine (RequestProductData ());
+
+		}
+	}
+
+	IEnumerator RequestProductData()
+	{
+		yield return null;
+
+		var productIdentifiers = new string[] { "com.joywinggames.MayDay.IosMedal011", "com.joywinggames.MayDay.IosMedal012", "com.joywinggames.MayDay.IosMedal013", "com.joywinggames.MayDay.IosMedal014", "com.joywinggames.MayDay.IosMedal015" };
+		StoreKitBinding.requestProductData( productIdentifiers );
+
+	}
+
+	public IEnumerator Purchase20()
+	{
+		yield return null;
+		loadingWindow.SetActive(true);
+		gameObject.GetComponent<StoreKitEventListener> ().productNumber = 1;
+		StoreKitBinding.purchaseProduct( "com.joywinggames.MayDay.IosMedal011", 1 );
+		Debug.Log( "preparing to purchase product: " + "com.joywinggames.MayDay.IosMedal011" );
+
+	}
+
+	public IEnumerator Purchase50()
+	{
+		yield return null;
+		loadingWindow.SetActive(true);
+		gameObject.GetComponent<StoreKitEventListener> ().productNumber = 2;
+		StoreKitBinding.purchaseProduct( "com.joywinggames.MayDay.IosMedal012", 1 );
+		Debug.Log( "preparing to purchase product: " + "com.joywinggames.MayDay.IosMedal012" );
+	}
+
+	public IEnumerator Purchase100()
+	{
+		yield return null;
+		loadingWindow.SetActive(true);
+		gameObject.GetComponent<StoreKitEventListener> ().productNumber = 3;
+		StoreKitBinding.purchaseProduct( "com.joywinggames.MayDay.IosMedal013", 1 );
+		Debug.Log( "preparing to purchase product: " + "com.joywinggames.MayDay.IosMedal013" );
+	}
+
+	public IEnumerator Purchase300()
+	{
+		yield return null;
+		loadingWindow.SetActive(true);
+		gameObject.GetComponent<StoreKitEventListener> ().productNumber = 4;
+		StoreKitBinding.purchaseProduct( "com.joywinggames.MayDay.IosMedal014", 1 );
+		Debug.Log( "preparing to purchase product: " + "com.joywinggames.MayDay.IosMedal014" );
+	}
+
+	public IEnumerator Purchase1000()
+	{
+		yield return null;
+		loadingWindow.SetActive(true);
+		gameObject.GetComponent<StoreKitEventListener> ().productNumber = 5;
+		StoreKitBinding.purchaseProduct( "com.joywinggames.MayDay.IosMedal015", 1 );
+		Debug.Log( "preparing to purchase product: " + "com.joywinggames.MayDay.IosMedal015" );
+	}
+
+#endif
+}
